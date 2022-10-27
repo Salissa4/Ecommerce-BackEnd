4c0a869a62361d80cdd8551c7f4bde4208270761
Monahlissa <salissa4@gmail.com>
2022-10-27 15:40:58 -0500

    update post route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,6 +16,7 @@ router.get('/', async (req, res) => {
   }
 });
 
+//LOOK THIS ONE UP MAY NOT BE RIGHT//
 router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   try {
@@ -32,9 +33,7 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new category
   try {
-    const newCategory = await Category.create({
-      category_id: req.body.category_id, //idk if this is right
-    });
+    const newCategory = await Category.create(req.body);
     res.status(200).json(newCategory);
   } catch (err) {
     res.status(400).json(err);
