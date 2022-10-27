4d867161b6455397169ce5bd239f1428ba71faa8
Monahlissa <salissa4@gmail.com>
2022-10-27 15:22:42 -0500

    created routes, need to test

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -41,12 +41,43 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      name: req.body.name,
+      id: req.body.id,
+    },
+    {
+      where: {
+        category_id: req.params.category_id,
+      },
+    }
+  ).then((updatedCategory) => {
+    res.json(updatedCategory);
+  }).catch((err) => {
+    console.log(err);
+    res.json(err);
+  });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!categoryData) {
+      res.status(404).json({message: 'No category found with that id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
 });
 
 module.exports = router;
